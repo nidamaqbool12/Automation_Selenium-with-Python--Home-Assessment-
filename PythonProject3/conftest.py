c4de975c44cf8a5b3eb785c7963d8fbb18f74f27
Nida Maqbool <84409898+nidamaqbool12@users.noreply.github.com>
2025-12-14 13:12:10 +0500

    Delete PythonProject3 directory

--- PythonProject3/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from selenium import webdriver
-
-@pytest.fixture
-def driver():
-    driver = webdriver.Chrome()
-    driver.set_window_size(1920, 1080)
-    yield driver
-    driver.quit()
